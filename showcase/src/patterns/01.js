0610d9f0d902d9d4b5fde1e13d81b4259df8817b
nicbongo <nicbongo@protonmail.com>
2020-07-22 14:16:12 -0300

    animation done

--- showcase/src/patterns/01.js
+++ showcase/src/patterns/01.js
@@ -20,36 +20,76 @@ const clapWithAnimation = (WrappedComponent) => {
     };
 
     componentDidMount() {
-      const tlDuration = 300
+      const tlDuration = 300;
       const scaleButton = new mojs.Html({
         el: "#clap",
         duration: tlDuration,
         scale: { 1.3: 1 },
         easing: mojs.easing.ease.out,
       });
+
+      const triangleBurst = new mojs.Burst({
+        parent: "#clap",
+        burst: { 50: 95 },
+        count: 5,
+        angle: 30,
+        children: {
+          shape: "polygon",
+          radius: { 6: 0 },
+          stroke: "rgba(211,54,0,0.5)",
+          strokeWidth: 2,
+          angle: 210,
+          delay: 30,
+          speed: 0.2,
+          easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
+          duration: tlDuration,
+        },
+      });
+
+      const circleBurst = new mojs.Burst({
+        parent: "#clap",
+        burst: { 50: 75 },
+        angle: 25,
+        duration: tlDuration,
+        children: {
+          shape: "circle",
+          fill: "rgba(149,165,166,0.5)",
+          delay: 30,
+          speed: 0.2,
+          radius: { 3: 0},
+          easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
+        },
+      });
+
       const countAnimation = new mojs.Html({
-        el: '#clapCount',
+        el: "#clapCount",
         opacity: { 0: 1 },
         duration: tlDuration,
-        y: { 0: -30}
+        y: { 0: -30 },
       }).then({
-        opacity: {1: 0},
+        opacity: { 1: 0 },
         y: -80,
         delay: tlDuration / 2,
-      })
+      });
 
       const countTotalAnimation = new mojs.Html({
-        el: '#clapCountTotal',
-        opacity: { 0 : 1 },
+        el: "#clapCountTotal",
+        opacity: { 0: 1 },
         delay: (3 * tlDuration) / 2,
         duration: tlDuration,
-        y: { 0: -5}
-      })
+        y: { 0: -5 },
+      });
 
-      const clap = document.getElementById('clap')
-      clap.style.transform = 'scale(1,1)'
+      const clap = document.getElementById("clap");
+      clap.style.transform = "scale(1,1)";
 
-      const newAnimationTimeline = this.animationTimeline.add([scaleButton, countTotalAnimation, countAnimation]);
+      const newAnimationTimeline = this.animationTimeline.add([
+        scaleButton,
+        countTotalAnimation,
+        countAnimation,
+        triangleBurst,
+        circleBurst
+      ]);
 
       this.setState({ animationTimeline: newAnimationTimeline });
     }
@@ -114,11 +154,20 @@ const ClapIcon = ({ isClicked }) => {
 };
 
 const ClapCount = ({ count }) => {
-  return <span id="clapCount" className={styles.count}> + {count}</span>;
+  return (
+    <span id="clapCount" className={styles.count}>
+      {" "}
+      + {count}
+    </span>
+  );
 };
 
 const CountTotal = ({ countTotal }) => {
-  return <span id="clapCountTotal" className={styles.total}>{countTotal}</span>;
+  return (
+    <span id="clapCountTotal" className={styles.total}>
+      {countTotal}
+    </span>
+  );
 };
 
 /* 
